415a22051a2bcad9487152bdf1927380e8857c4c
tiNa <nathnaelmengistu13@gmail.com>
2023-08-04 18:13:31 +0300

    change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const hoursEl=document.getElementById("hours");
 const minsEl=document.getElementById("mins");
 const secondsEl=document.getElementById("seconds");
 
-const postdate="4 augest 2023";
+const postdate="4 augest 2023"; 
 
 function countdown(){
     const postdateD=new Date(postdate);
