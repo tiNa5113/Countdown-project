a8333e0dfab6fb76c938aa3b003b883dcc56cd4d
tiNa <nathnaelmengistu13@gmail.com>
2023-08-10 08:24:27 +0300

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const hoursEl=document.getElementById("hours");
 const minsEl=document.getElementById("mins");
 const secondsEl=document.getElementById("seconds");
 
-const postdate="8 augest 2023"; 
+const postdate="9 augest 2023"; 
 
 function countdown(){
     const postdateD=new Date(postdate);
