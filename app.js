a876e8ceeb6b6b9b3481aa18c7b02a9c931fae21
tiNa5113 <nathnaelmengistu13@gmail.com>
2024-01-22 10:48:51 +0300

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const postdate = "23 jan 2024";
+const postdate = "24 jan 2024";
 
 function countdown() {
   const postdateD = new Date(postdate);
